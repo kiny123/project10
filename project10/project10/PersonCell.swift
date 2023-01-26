4a338e5c1ab53f2404970e301ff91ce134c3f6c5
Kiny <89192734912nik@gmail.com>
2023-01-27 01:19:31 +0300

    end day 41

---
+++ project10/project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by nikita on 27.01.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
